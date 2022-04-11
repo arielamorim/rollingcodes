c0066f86e99ce04d5f1d13d411905910aa4de269
Ariel <arielf.amorim@gmail.com>
2022-04-11 14:48:31 -0300

    Born this way

---
+++ index.js
@@ -0,0 +1,47 @@
+const intervals = {
+    major: ['T','T','sT','T','T','T','sT'],
+    minor: ['T','sT','T','T','sT','T','T'],
+    values: { T: 2, sT: 1}
+};
+
+let allNotes = ['C', 'C#', 'D', 'D#','E','F','F#','G','G#','A','A#','B'];
+
+function getScale ( tom, variation ) {
+    
+    // Order notes by tom
+    let tomIndex = allNotes.indexOf(tom);
+    
+    // Define were to begin
+    let allNotesEnd = [], allNotesStart = [];
+    allNotes.forEach( (note, index) => {
+        if ( index >= tomIndex ) {
+            allNotesStart.push(note);
+        } else {
+            allNotesEnd.push(note);
+        }
+    });
+
+    // Get notes ordered
+    let notesOrdered = allNotesStart.concat(allNotesEnd);
+    
+    // Crossing the intervals template with allNotes
+    let scale = [ tom ];
+
+    // Don't know how to explaine the use this, so...
+    let badAssIndex = 0;
+    
+    // Magic
+    intervals[`${variation}`].forEach(( interval ) => {
+        
+        badAssIndex = badAssIndex + intervals.values[`${interval}`];
+        
+        notesOrdered[badAssIndex] = badAssIndex === notesOrdered.length ? tom : notesOrdered[badAssIndex];
+        
+        scale.push(notesOrdered[badAssIndex]);
+        
+    });
+
+    return scale;
+}
+
+console.log('There you go:', getScale('F', 'major'));
